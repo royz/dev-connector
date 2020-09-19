9ffe2db4b9f16e6bdfd0b3dcfcf127db3ad801f3
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-09-20 02:09:35 +0530

    added profile model

---
+++ models/Profile.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+
+
+const Profile = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String,
+  },
+  githubUsername: {
+    type: String,
+  },
+  experience: [{
+    title: {
+      type: String,
+      require: true
+    },
+    company: {
+      type: String,
+      require: true
+    },
+    location: {
+      type: String,
+    },
+    from: {
+      type: Date,
+      require: true
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String
+    }
+  }],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldOfStudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
